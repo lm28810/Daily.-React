be8ebf8ad614d9c662e00fe3c47e7ef079c468c6
Jazmine <lm28810@gmail.com>
2022-09-08 22:14:32 -0400

    messy props

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,25 +1,42 @@
+import { Component } from 'react'
 import logo from './logo.svg';
+import Props from './Props'
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      name: "Jazmine",
+      company: "Dorreans House",
+    }
+  }
+changeTxt(){
+  this.setState({
+    name:"Dorrean",
+    company: "Jazmine's House"
+  })
+}
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Props name="Dorrean" company="HenryFord Health System" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {this.state.name} would you like to work at {this.state.company}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=> this.changeTxt()}>
+          Change the Text
+        </button>
+        
       </header>
+
     </div>
   );
+  }
 }
 
 export default App;
---
+++ practice/src/Props.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Props(props) {
+  return (
+    <div>
+
+<h1>Hello {props.name} thank you for visiting {props.company}</h1>
+
+
+    </div>
+  )
+}
+
+export default Props
